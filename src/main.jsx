8a4a1988b378a9504a7e68a9e1e5e01f063242c0
YOKSAN <poumianyoks@gmail.com>
2026-09-22 20:38:38 -0600

    Agregar Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
+  <Analytics />
+</StrictMode>,
 )
